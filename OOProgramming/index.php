7677e697999a7e387efef0e454e924f8a3738b84
Bernie <bernardo.menendez@i-surance.es>
2020-08-27 09:59:13 +0200

    Remove comment in index.php

--- OOProgramming/index.php
+++ OOProgramming/index.php
@@ -5,9 +5,6 @@ require __DIR__ . '/../bootstrap.php';
 /**
  * You can start writing code here. Please do not change any skeleton classes.
  * You can create as many files and folders and namespaces as you need
- *
- * You might notice that we are using Symfony2 UniversalClassLoader. Do not worry if you have
- * never used Symfony2, if you follow PSR-0 it will just work as expected.
  */
 use Isurance\OOProgramming\Dummy;
 
