ffdbfdca4a83002d83808b7b3ce4d88afffa8b40
Bernie <bernardo.menendez@i-surance.es>
2020-08-27 09:53:21 +0200

    Modified autoload and namespaces and remove old autoload

--- Testing/StringTools.php
+++ Testing/StringTools.php
@@ -7,7 +7,7 @@ class StringTools
     public function capitalize($str)
     {
         if (!is_scalar($str) || is_numeric($str)) {
-            throw new InvalidArgumentException('Capitalize only accepts strings!');
+            throw new \InvalidArgumentException('Capitalize only accepts strings!');
         }
 
         return ucwords(strtolower($str));
